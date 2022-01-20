3a8ea6527e0cac6557bedf5979a5836b1f094cab
tmgasek <tomaszgasek28@gmail.com>
2022-01-20 12:44:25 +0000

    user registration working

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const LoginPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (email, password) => {
+    console.log(email, password);
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <div>
+        <h1>Supabase + Next.js</h1>
+        <p>Sign in via magic link with your email below</p>
+        <div>
+          <input
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogin(email, password);
+            }}
+            disabled={loading}
+          >
+            <span>{loading ? 'Loading' : 'Send magic link'}</span>
+          </button>
+        </div>
+        <div>
+          <h4>Don&apos;t have an account?</h4>
+          <Link href={'/register'}>
+            <a>Register</a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
+import { supabase } from '../lib/initSupabase';
 
 const RegisterPage = () => {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const emailEl = useRef();
+  const passwordEl = useRef();
+  const confirmPasswordEl = useRef();
+
+  const handleRegister = async () => {
+    const { value: email } = emailEl.current;
+    const { value: password } = passwordEl.current;
+    const { value: confirmPassword } = confirmPasswordEl.current;
+
+    if (password !== confirmPassword) {
+      //show error
+      console.log('password mismatch!');
+      return;
+    }
+    //create the user with supabase
+    try {
+      const { user, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+      //if there's an error received from supabase, we throw it to catch it in the catch block.
+      if (error) throw error;
+      router.push('/login');
+    } catch (error) {
+      //show error
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Register</h1>
+      <div>
+        <h1>Supabase + Next.js</h1>
+        <p>Sign in via magic link with your email below</p>
+        <div>
+          <input type="email" placeholder="Your email" ref={emailEl} />
+        </div>
+        <div>
+          <input type="password" placeholder="password" ref={passwordEl} />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="confirm password"
+            ref={confirmPasswordEl}
+          />
+        </div>
+
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleRegister();
+            }}
+            disabled={loading}
+          >
+            <span>{loading ? 'Loading' : 'Send magic link'}</span>
+          </button>
+        </div>
+        <div>
+          <h4>Already registered?</h4>
+          <Link href={'/login'}>
+            <a>Log in</a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
